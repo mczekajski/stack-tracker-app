1303dce6413165fbe5cbed81a09a9a146092a982
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-01-25 16:56:28 +0100

    login form with validators

---
+++ src/app/pages/login/login-form/login-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(255), Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(255)])
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(): void {
+    console.log('login')
+  }
+
+}
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, LoginFormComponent],
   imports: [
-    CommonModule
-  ],
-  exports: [LoginComponent]
+    CommonModule, ReactiveFormsModule
+  ]
 })
 export class LoginModule { }
