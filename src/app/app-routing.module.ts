3491b1ca57cdbd3f0e074b6dfe1005c24391632f
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-01-26 22:22:26 +0100

    try fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: '**', pathMatch: 'full', component: AppComponent }
 ];
 
 @NgModule({
